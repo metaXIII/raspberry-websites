7799dc9d950e1a46e71702e2cae0a9a21258d635
metaxiii <lehchibi.gael@pm.me>
2022-03-10 19:49:35 +0100

    add first part

---
+++ game-manager/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="../public/css/index.css"/>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/public/css/index.css">
+        <title>Game Manager</title>
+    </head>
+    <body>
+        <header class="menu dark-background">
+            <ul class="menu-list">
+                <li class="menu-item">
+                    <a href="/" class="item-link">Retour sur mes projets</a>
+                </li>
+            </ul>
+        </header>
+        <main class="main">
+            <p>Mon content</p>
+        </main>
+        <footer class="footer menu dark-background">
+            <p class="main text-right">
+                All right reserved
+            </p>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="public/css/index.css"/>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Mes sites sur Raspberry</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+                <a class="navbar-brand" href="/">Navbar</a>
+            </nav>
+        </header>
+        <main class="main">
+            <div class="container">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Mes jeux</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam atque
+                            dolore, explicabo
+                            facilis fuga ipsam minima non nostrum possimus quidem recusandae repudiandae vero! Deserunt
+                            ipsam maxime optio quia tempora?</p>
+                        <a href="" class="btn btn-primary">Aller sur le site</a>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="footer menu dark-background">
+            <p class="main text-right">
+                All right reserved
+            </p>
+        </footer>
+    </body>
+</html>
